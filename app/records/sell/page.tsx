9a15fcaeb410cae0caddd1aaef4a7f7c495d5be7
baharehforoughi <bhr.foroughi@gmail.com>
2024-03-14 13:45:02 +0100

    Fix error in Sell Records

--- app/records/sell/page.tsx
+++ app/records/sell/page.tsx
@@ -3,7 +3,7 @@
 import { Heading1 } from "@/components/typography";
 
 export default function SellRecords() {
-  // throw new Error("Whoops! An error in Sell Records happeneed!"); // <--- add this!
+  throw new Error("Whoops! An error in Sell Records happeneed!"); // <--- add this!
 
   return <Heading1>Sell Records</Heading1>;
 }
